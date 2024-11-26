a2029a80e9fe45382c5d79a9cd6df736e9af7e72
Uni <00hubertb00@gmail.com>
2024-11-26 23:47:44 +0100

    Init

---
+++ src/ParkingApp.API/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ParkingApp.API.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
